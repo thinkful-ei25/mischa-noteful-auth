1e626408ae678d3da16efdf7c983e987116c99f1
mischb <mischarberlin@gmail.com>
2018-10-29 18:13:42 -0400

    test

---
+++ models/user.js
@@ -0,0 +1,30 @@
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  fullName: { type: String },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+
+// Transform output during `res.json(data)`, `console.log(data)` etc.
+userSchema.set('toJSON', {
+  virtuals: true,     // include built-in virtual `id`
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+    delete result.password;
+  }
+});
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, 10);
+};
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
+
+
+module.exports = mongoose.model('User', userSchema);
---
+++ routes/users.js
@@ -0,0 +1,117 @@
+
+const express = require('express');
+// const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  User.find()
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/', (req, res, next) => {
+  const { fullName, username, password } = req.body;
+
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+  // const newUser = { fullName, username, password  };
+
+  if(missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field', 
+      location: missingField
+    });
+  }
+
+  const stringFields = ['username', 'password', 'fullName'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  const explicitlyTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicitlyTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if(nonTrimmedField) {
+    return res.status(422).json({
+      code: 422, 
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8, 
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field => 
+      'min' in sizedFields[field] && req.body[field].trim().length < sizedFields[field].min
+  );
+
+  const tooLargeField = Object.keys(sizedFields).find(
+    field => 
+      'max' in sizedFields[field] && req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField ?  `Must be at least ${sizedFields[tooSmallField].min} characters long` : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  User.find({username})
+    .then(() => {
+      return User.hashPassword(password)
+        .then(digest => {
+          const newUser = {
+            username, 
+            password: digest, 
+            fullName
+          };
+          return User.create(newUser);
+        })
+        .then(result => {
+          res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+        })
+    
+        .catch(err => {
+          if (err.code === 11000) {
+            err = new Error('username already exists');
+            err.status = 400;
+          }
+          next(err);
+        });
+    });
+});
+module.exports = router;
